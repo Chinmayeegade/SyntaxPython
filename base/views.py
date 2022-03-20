e580f1e28bcb6879b7e1e36fd6b232b4d8502660
Chinmayeegade <chinmayeepgade@gmail.com>
2022-03-20 13:48:14 +0530

    Initial Commit

---
+++ base/views.py
@@ -0,0 +1,228 @@
+from django.shortcuts import render
+
+
+
+def home(request):
+    return render(request, "home.html")
+
+def topics(request):
+    return render(request, "topics.html")
+
+def projects(request):
+    return render(request, "projects.html")
+
+def packages(request):
+    return render(request, "packages.html")
+
+def facts(request):
+    return render(request, "facts.html")
+
+def a(request):
+    return render(request, "a.html")
+
+
+def b(request):
+    return render(request, "b.html")
+
+
+def c(request):
+    return render(request, "c.html")
+
+
+def d(request):
+    return render(request, "d.html")
+
+
+def e(request):
+    return render(request, "e.html")
+
+
+def f(request):
+    return render(request, "f.html")
+
+
+def g(request):
+    return render(request, "g.html")
+
+
+def h(request):
+    return render(request, "h.html")
+
+
+def i(request):
+    return render(request, "i.html")
+
+
+def j(request):
+    return render(request, "j.html")
+
+
+def k(request):
+    return render(request, "k.html")
+
+
+def l(request):
+    return render(request, "l.html")
+
+
+def m(request):
+    return render(request, "m.html")
+
+
+def n(request):
+    return render(request, "n.html")
+
+
+def o(request):
+    return render(request, "o.html")
+
+
+def p(request):
+    return render(request, "p.html")
+
+
+def q(request):
+    return render(request, "q.html")
+
+
+def r(request):
+    return render(request, "r.html")
+
+
+def s(request):
+    return render(request, "s.html")
+
+
+def t(request):
+    return render(request, "t.html")
+
+
+def u(request):
+    return render(request, "u.html")
+
+
+def v(request):
+    return render(request, "v.html")
+
+
+def w(request):
+    return render(request, "w.html")
+
+
+def x(request):
+    return render(request, "x.html")
+
+
+def y(request):
+    return render(request, "y.html")
+
+
+def z(request):
+    return render(request, "z.html")
+
+
+def AA(request):
+    return render(request, "AA.html")
+
+
+def BB(request):
+    return render(request, "BB.html")
+
+
+def CC(request):
+    return render(request, "CC.html")
+
+
+def DD(request):
+    return render(request, "DD.html")
+
+
+def EE(request):
+    return render(request, "EE.html")
+
+
+def FF(request):
+    return render(request, "FF.html")
+
+
+def GG(request):
+    return render(request, "GG.html")
+
+
+def H(request):
+    return render(request, "H.html")
+
+
+def I(request):
+    return render(request, "I.html")
+
+
+def J(request):
+    return render(request, "J.html")
+
+
+def K(request):
+    return render(request, "K.html")
+
+
+def L(request):
+    return render(request, "L.html")
+
+
+def M(request):
+    return render(request, "M.html")
+
+
+def N(request):
+    return render(request, "N.html")
+
+
+def O(request):
+    return render(request, "O.html")
+
+
+def P(request):
+    return render(request, "P.html")
+
+
+def Q(request):
+    return render(request, "Q.html")
+
+
+def R(request):
+    return render(request, "R.html")
+
+
+def S(request):
+    return render(request, "S.html")
+
+
+def T(request):
+    return render(request, "T.html")
+
+
+def U(request):
+    return render(request, "U.html")
+
+
+def V(request):
+    return render(request, "V.html")
+
+
+def W(request):
+    return render(request, "W.html")
+
+
+def X(request):
+    return render(request, "X.html")
+
+
+def Y(request):
+    return render(request, "Y.html")
+
+
+def Z(request):
+    return render(request, "Z.html")
+
+def sidebar(request):
+    return render(request, "sidebar.html")
